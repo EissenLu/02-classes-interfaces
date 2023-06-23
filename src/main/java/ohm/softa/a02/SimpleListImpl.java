072c38272eb7da3397d1ccf512742fdbc4fbdcf0
EissenLu <lukas.eissen@yahoo.de>
2023-06-23 18:04:30 +0200

    Impl. static Element Class

--- src/main/java/ohm/softa/a02/SimpleListImpl.java
+++ src/main/java/ohm/softa/a02/SimpleListImpl.java
@@ -1,12 +1,40 @@
 package ohm.softa.a02;
 
+import java.util.Iterator;
+
 /**
  * @author Peter Kurfer
  * Created on 10/6/17.
  */
 public class SimpleListImpl implements SimpleList {
 
-	// TODO: Implement the required methods.
+	private static class Element{
+        Object o;
+        Element next;
+        Element(Object o){
+            this.o = o;
+            this.next = null;
+        }
+
+        Element getNext() {return this.next; }
+        void setNext(Element e) {this.next = e; }
+        Object getObj() {return this.o;}
+        void setObj(Object o) {this.o = o;}
+
+    }
+
+    public class SimplIteratorImpl implements Iterator {
+
+        @Override
+        public boolean hasNext() {
+            return false;
+        }
+
+        @Override
+        public Object next() {
+            return null;
+        }
+    }
 
 
 }
